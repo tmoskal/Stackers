cea55eb45ed7ffe510a24c6a8c32a1eb242b007d
Tate Moskal <tate.moskal@gmail.com>
2018-07-25 15:44:15 -0500

    full game

--- scripts/stackers.py
+++ scripts/stackers.py
@@ -6,6 +6,7 @@ from getpass import getpass
 sense = SenseHat()
 sense.clear()
 colors = [(225,0,0),(255,170,0),(255,255,0),(0,255,0),(0,0,255),(119,0,179),(255,0,214),(255,0,0)]
+rows = []
 class stack():
 	def __init__(self):
 		pygame.init()
@@ -18,24 +19,59 @@ class stack():
 		speed = .3
 		y = 7
 		n = 0
+		p = 0
+		z = 0
 		while self.gaming:
 		
 			for event in pygame.event.get():
 				if event.type == KEYDOWN:
 					if y == -1:
-						exit()				
+						sense.clear()
+						sense.show_message("Winner", 0.1, text_colour=(0,255,0), back_colour=(0,0,0))
+						sense.clear()
+						exit()	
+					if y == 7:
+						rows.append(x)
+						p = p + 1
 					if x == 0:
 						x = 8
 						sense.set_pixel(x-1,y,(colors[n]))
 						y = y-1
 						x = 0
 						n = n + 1
+						
+						
 					else:
 						sense.set_pixel(x-1,y,colors[n])
 						y = y-1
 						n = n + 1
+			
+					
+					if(y < 7):
+						rows.append(x)
+						if(rows[p] > rows[z]):
+							time.sleep(2)
+							sense.clear()
+							sense.show_message("Game Over", 0.1, text_colour=(0,255,0), back_colour=(0,0,0))
+							sense.clear()
+							exit()
+						elif(rows[p] < rows[z]):
+							time.sleep(2)
+							sense.clear()
+							sense.show_message("Game Over", 0.1, text_colour=(0,255,0), back_colour=(0,0,0))
+							sense.clear()
+							exit()
+						else:
+							p = p + 1
+							z = z + 1		
+						
+
+					
 				else:
 					if y == -1:
+						sense.clear()
+						sense.show_message("Winner", 0.05, text_colour=(0,255,0), back_colour=(0,0,0))
+						sense.clear()
 						exit()
 					sense.set_pixel(x,y,(225,225,225))
 					time.sleep(speed)
@@ -45,7 +81,6 @@ class stack():
 						x = x + 1
 					if x == 8:
 						x = 0
-				
 				
 if __name__ == '__main__' : 
 	try:
